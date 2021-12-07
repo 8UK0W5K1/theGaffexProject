107e16880b4632e8098fafd2e8a6181d34f6b636
talmidiel <talmidiel.officiel@gmail.com>
2021-12-07 09:47:16 +0100

    created the migration

--- db/migrate/20211207084245_create_articles.rb
+++ db/migrate/20211207084245_create_articles.rb
@@ -1,6 +1,13 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
+      t.belongs_to :user
+      t.string :title
+      t.text :introduction
+      t.text :protocol
+      t.text :result
+      t.text :conclusion
+      t.text :references
 
       t.timestamps
     end
