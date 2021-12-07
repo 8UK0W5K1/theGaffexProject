cac84f37a977476567a0f9fc3b7b7c260446db9e
talmidiel <talmidiel.officiel@gmail.com>
2021-12-07 16:48:22 +0100

    base setupo for sendgrid smtp

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the Rails application.
 require_relative 'application'
 
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_PUBLIC'],
+  password: ENV['SENDGRID_SECRET'],
+  domain: 'gaffex.dev@gmail.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 # Initialize the Rails application.
 Rails.application.initialize!
