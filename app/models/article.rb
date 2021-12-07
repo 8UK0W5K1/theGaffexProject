f753db611520444210b9868ded93ab94e09437aa
talmidiel <talmidiel.officiel@gmail.com>
2021-12-07 09:55:05 +0100

    added validates to models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :tags, dependent: :destroy
+
+  validates_presence_of :title, :introduction, :protocol, :result, :conclusion, :references
+  validates :title, length: { minimum: 5, maximum: 140 }
 end
