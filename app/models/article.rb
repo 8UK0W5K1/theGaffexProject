ededa81599e2346ce15b417eb067700ce5205e3a
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-07 20:02:55 +0100

    first version of front article index

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,15 @@ class Article < ApplicationRecord
 
   validates :title, :summary, :introduction, :protocol, :result, :conclusion, :references, presence: true
   validates :title, length: { minimum: 5, maximum: 140 }
+
+
+  def   short_summary
+    short_summary = ""
+    word_array = self.summary.split("")
+    70.times do |index|
+      short_summary += word_array[index]
+    end
+    short_summary +=  " ... "
+    short_summary
+  end
 end
