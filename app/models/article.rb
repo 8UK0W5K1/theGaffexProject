7f64f2d79c3c88ac16e403b3f6da8d8befe59d98
talmidiel <talmidiel.officiel@gmail.com>
2021-12-07 09:56:29 +0100

    changed the validate style for rubocop

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :tags, dependent: :destroy
 
-  validates_presence_of :title, :introduction, :protocol, :result, :conclusion, :references
+  validates :title, :introduction, :protocol, :result, :conclusion, :references, presence: true
   validates :title, length: { minimum: 5, maximum: 140 }
 end
