06cf8e95a22f10366f22de44f5a8427b8710d79f
talmidiel <talmidiel.officiel@gmail.com>
2021-12-07 16:53:00 +0100

    created the welcome mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'gaffex.dev@gmail.com'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'https://the-gaffex-project.herokuapp.com/users/sign_in'
+    mail(to: @user.email, subjet: 'bienvenue sur Gaffex !')
+  end
 end
