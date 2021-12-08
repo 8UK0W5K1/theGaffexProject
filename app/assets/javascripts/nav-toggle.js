d6a91506d6034c8a81a114da63466356e7eb598c
talmidiel <talmidiel.officiel@gmail.com>
2021-12-08 01:00:04 +0100

    made the js for the navbar

---
+++ app/assets/javascripts/nav-toggle.js
@@ -0,0 +1,25 @@
+function unFade(element) {
+    let op = 0.1;  // initial opacity
+    element.style.opacity = op;
+    element.style.display = 'flex';
+    let timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 20);
+}
+
+document.querySelector('#nav-closed-toggler').addEventListener('click', () => {
+    document.querySelector('#nav-closed').style.display = 'none';
+    document.querySelector('#nav-closed').classList.remove('d-flex');
+    unFade(document.querySelector('#nav-opened'))
+})
+
+document.querySelector('#nav-opened-toggler').addEventListener('click', () => {
+    document.querySelector('#nav-opened').style.display = 'none';
+    document.querySelector('#nav-opened').classList.remove('d-flex');
+    unFade(document.querySelector('#nav-closed'))
+})
