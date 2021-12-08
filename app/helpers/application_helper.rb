322932f46157a29bde47565a8a45e0cbf230ef64
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-08 19:43:23 +0100

    create heper for count tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def tag_count(article)
+        number_approved = article.tags.where(tag_name: "Approved").length
+        number_not_approved = article.tags.where(tag_name: "Not Approved").length
+        number_need_rewiew = article.tags.where(tag_name: "Need Rewiew").length       
+        
+        tag_count = {approved: number_approved, not_approved: number_not_approved, need_rewiew: number_need_rewiew}
+    end
 end
