24cef08431c4a478098a54559b618862181cf078
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-08 20:06:31 +0100

    fix orthography need_rewiew to need_review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,8 @@ module ApplicationHelper
     def tag_count(article)
         number_approved = article.tags.where(tag_name: "Approved").length
         number_not_approved = article.tags.where(tag_name: "Not Approved").length
-        number_need_rewiew = article.tags.where(tag_name: "Need Rewiew").length       
+        number_need_review = article.tags.where(tag_name: "Need Review").length       
         
-        tag_count = {approved: number_approved, not_approved: number_not_approved, need_rewiew: number_need_rewiew}
+        tag_count = {approved: number_approved, not_approved: number_not_approved, need_review: number_need_review}
     end
 end
