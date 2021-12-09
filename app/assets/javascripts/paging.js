c6c4f43dc1299f3f586b4382a3f4bb0a31ab45e4
talmidiel <talmidiel.officiel@gmail.com>
2021-12-09 17:08:01 +0100

    first version of the script

--- app/assets/javascripts/paging.js
+++ app/assets/javascripts/paging.js
@@ -1,27 +1,51 @@
-let params = new URLSearchParams(location.search);
-switch (params.get('page')) {
-    case null:
-        document.querySelector('#btn--1').firstElementChild.textContent = '1';
-        document.querySelector('#btn--1').firstElementChild.href = ' '
-        document.querySelector('#btn-0').firstElementChild.textContent = '2';
-        document.querySelector('#btn-1').firstElementChild.textContent = '3';
-        document.querySelector('#btn-2').firstElementChild.textContent = '4';
-        document.querySelector('#btn-3').firstElementChild.textContent = '5';
-        break
-    case '1':
-        document.querySelector('#btn-' + (params.get('page') - 2)).firstElementChild.textContent = '1'
-        document.querySelector('#btn-' + (params.get('page') - 1)).firstElementChild.textContent = '2'
-        document.querySelector('#btn-' + params.get('page')).firstElementChild.textContent = '3'
-        document.querySelector('#btn-' + (params.get('page') + 1)).firstElementChild.textContent = '4'
-        document.querySelector('#btn-' + (params.get('page') + 2)).firstElementChild.textContent = '5'
-        break
-    case '2':
-        document.querySelector('#btn-' + (params.get('page') - 2)).firstElementChild.textContent = '2'
-        document.querySelector('#btn-' + (params.get('page') - 1)).firstElementChild.textContent = '3'
-        document.querySelector('#btn-' + params.get('page')).firstElementChild.textContent = '4'
-        document.querySelector('#btn-' + (params.get('page') + 1)).firstElementChild.textContent = '5'
-        document.querySelector('#btn-' + (params.get('page') + 2)).firstElementChild.textContent = '6'
-        break
-}
+class Paging {
+    constructor(ul, url, page) {
+        this.ul = ul;
+        this.url = url;
+        this.page = parseInt(page);
+        this.btn1 = ul.querySelector('#btn-1');
+        this.btn2 = ul.querySelector('#btn-2');
+        this.btn3 = ul.querySelector('#btn-3');
+        this.btn4 = ul.querySelector('#btn-4');
+        this.btn5 = ul.querySelector('#btn-5');
 
-if (params.get('page') !== null && params.get('page') > 2) document.querySelector('#btn-' + params.get('page')).classList.add('active')
+    }
+
+    pageNull() {
+        this.btn1.firstElementChild.textContent = '1'
+        this.btn2.firstElementChild.textContent = '2'
+        this.btn3.firstElementChild.textContent = '3'
+        this.btn4.firstElementChild.textContent = '4'
+        this.btn5.firstElementChild.textContent = '5'
+    }
+
+    pageOne() {
+        this.btn1.firstElementChild.textContent = this.page.toString()
+        this.btn2.firstElementChild.textContent = (this.page + 1).toString()
+        this.btn3.firstElementChild.textContent = (this.page + 2).toString()
+        this.btn4.firstElementChild.textContent = (this.page + 3).toString()
+        this.btn5.firstElementChild.textContent = (this.page + 4).toString()
+    }
+
+    pageTwo() {
+        this.btn1.firstElementChild.textContent = (this.page - 1).toString()
+        this.btn2.firstElementChild.textContent = (this.page).toString()
+        this.btn3.firstElementChild.textContent = (1 + this.page).toString()
+        this.btn4.firstElementChild.textContent = (2 + this.page).toString()
+        this.btn5.firstElementChild.textContent = (3 + this.page).toString()
+    }
+
+    init() {
+        switch (this.page) {
+            case null:
+                this.pageNull();
+                break
+            case 1:
+                this.pageOne();
+                break
+            case 2:
+                this.pageTwo();
+                break
+        }
+    }
+}
