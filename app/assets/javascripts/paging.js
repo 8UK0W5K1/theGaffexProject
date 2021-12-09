3f6bd20fcb92c39d4ce1924bc1cf4379d450fd1b
talmidiel <talmidiel.officiel@gmail.com>
2021-12-09 17:37:01 +0100

    redone all the html link_to becausqe it was easier to work with it, redone the script according to this

--- app/assets/javascripts/paging.js
+++ app/assets/javascripts/paging.js
@@ -12,64 +12,22 @@ class Paging {
 
     }
 
-    pageNullUrl() {
-        this.btn1.firstElementChild.href = this.url + '1';
-        this.btn2.firstElementChild.href = this.url + '2';
-        this.btn3.firstElementChild.href = this.url + '3';
-        this.btn4.firstElementChild.href = this.url + '4';
-        this.btn5.firstElementChild.href = this.url + '5';
-    }
-
     pageNull() {
-        this.btn1.firstElementChild.textContent = '1'
-        this.btn2.firstElementChild.textContent = '2'
-        this.btn3.firstElementChild.textContent = '3'
-        this.btn4.firstElementChild.textContent = '4'
-        this.btn5.firstElementChild.textContent = '5'
         this.btn1.classList.add('active');
-        this.btnPrev.classList.add('disabled')
-        this.pageNullUrl()
-    }
-
-    pageOneUrl() {
-        this.btn1.firstElementChild.href = this.url + this.page.toString();
-        this.btn2.firstElementChild.href = this.url + (this.page + 1).toString();
-        this.btn3.firstElementChild.href = this.url + (this.page + 2).toString();
-        this.btn4.firstElementChild.href = this.url + (this.page + 3).toString();
-        this.btn5.firstElementChild.href = this.url + (this.page + 4).toString();
     }
 
     pageOne() {
-        this.btn1.firstElementChild.textContent = this.page.toString()
-        this.btn2.firstElementChild.textContent = (this.page + 1).toString()
-        this.btn3.firstElementChild.textContent = (this.page + 2).toString()
-        this.btn4.firstElementChild.textContent = (this.page + 3).toString()
-        this.btn5.firstElementChild.textContent = (this.page + 4).toString()
         this.btn1.classList.add('active');
-        this.btnPrev.classList.add('disabled')
-        this.pageOneUrl()
-    }
-
-    pageTwoUrl() {
-        this.btn1.firstElementChild.href = this.url + (this.page - 1).toString();
-        this.btn2.firstElementChild.href = this.url + (this.page).toString();
-        this.btn3.firstElementChild.href = this.url + (1 + this.page).toString();
-        this.btn4.firstElementChild.href = this.url + (2 + this.page).toString();
-        this.btn5.firstElementChild.href = this.url + (3 + this.page).toString();
     }
 
     pageTwo() {
-        this.btn1.firstElementChild.textContent = (this.page - 1).toString()
-        this.btn2.firstElementChild.textContent = (this.page).toString()
-        this.btn3.firstElementChild.textContent = (1 + this.page).toString()
-        this.btn4.firstElementChild.textContent = (2 + this.page).toString()
-        this.btn5.firstElementChild.textContent = (3 + this.page).toString()
         this.btn2.classList.add('active');
-        this.pageTwoUrl()
+        this.btnPrev.classList.remove('disabled');
     }
 
     pageDefault() {
         this.btn3.classList.add('active');
+        this.btnPrev.classList.remove('disabled');
         this.btn1.firstElementChild.href = this.url + (this.page - 2).toString();
         this.btn2.firstElementChild.href = this.url + (this.page - 1).toString();
         this.btn3.firstElementChild.href = this.url + this.page.toString();
@@ -77,12 +35,20 @@ class Paging {
         this.btn5.firstElementChild.href = this.url + (this.page + 2).toString();
     }
 
+    pageDefaultText() {
+        this.btn1.firstElementChild.textContent = (this.page - 2).toString();
+        this.btn2.firstElementChild.textContent = (this.page - 1).toString();
+        this.btn3.firstElementChild.textContent = (this.page).toString();
+        this.btn4.firstElementChild.textContent = (this.page + 1).toString();
+        this.btn5.firstElementChild.textContent = (this.page + 2).toString();
+    }
+
     init() {
-        console.log(this.page);
+        if (isNaN(this.page)) {
+            this.pageNull();
+            return
+        }
         switch (this.page) {
-            case NaN:
-                this.pageNull();
-                break
             case 1:
                 this.pageOne();
                 break
@@ -90,7 +56,8 @@ class Paging {
                 this.pageTwo();
                 break
             default:
-                this.pageDefault()
+                this.pageDefault();
+                this.pageDefaultText();
         }
     }
 }
