8c27fb1bfe152dd52028113a1070977bc9234864
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-09 15:29:48 +0100

    create destroy method in tag controller and apply in article show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,11 @@
 class TagsController < ApplicationController
     def create 
+        redirect_to root_path
+    end
+
+    def destroy
+        @tag = Tag.find(params[:id])
+        @tag.destroy
+        redirect_to article_path(params[:article_id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
   resources :articles do
-    resources :tags, only: %i[create] 
+    resources :tags, only: %i[create destroy] 
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
