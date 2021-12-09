37f05e73a81898ba4a84510d5067128d16a431ce
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-09 16:12:42 +0100

    update create in tagsController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,19 @@
 class TagsController < ApplicationController
     def create 
-        redirect_to root_path
+        if user_signed_in?
+
+            @article = Article.find(params[:article_id])
+            @tag = @article.tags.find_by(user: current_user)            
+            if @tag.nil?
+                @tag = Tag.create(user: current_user, article: @article, tag_name: 'Approved')
+            else
+                @tag.update(tag_name:'Approved')
+            end
+
+            redirect_to article_path(params[:article_id])
+        else
+            redirect_to new_user_session_path
+        end
     end
 
     def destroy
