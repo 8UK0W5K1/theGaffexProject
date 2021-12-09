59d7c13bd3f554f1b1d47e6b761972535b57191b
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-09 16:50:29 +0100

    update tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,9 +5,9 @@ class TagsController < ApplicationController
             @article = Article.find(params[:article_id])
             @tag = @article.tags.find_by(user: current_user)            
             if @tag.nil?
-                @tag = Tag.create(user: current_user, article: @article, tag_name: 'Approved')
+                @tag = Tag.create(user: current_user, article: @article, tag_name: params[:tag_name])
             else
-                @tag.update(tag_name:'Approved')
+                @tag.update(tag_name: params[:tag_name])
             end
 
             redirect_to article_path(params[:article_id])
