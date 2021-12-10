a343354372fb4c43110f0ba6d01d3dfc16daab8d
anaissiananann <anais.thibaudin@gmail.com>
2021-12-10 16:01:57 +0100

    user controller with edit & update created && link to show under profil in navbar x2

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
   
   resources :searches, only: %i[index]
+  resources :users, only: %i[edit update]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
