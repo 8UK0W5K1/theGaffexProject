8381ae4a244a032b524b2b990970c4e9d32b6be2
anaissiananann <anais.thibaudin@gmail.com>
2021-12-10 17:04:17 +0100

    update form in usere edit &update update method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   before_action :authenticate_user!
 
   def edit
@@ -7,6 +7,9 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user= current_user
+    @user.update(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to root_path
   end
   
 end
