37ae246ba5cad4ad05fafb03fb910507dd3e3b62
anaissiananann <anais.thibaudin@gmail.com>
2021-12-10 18:00:40 +0100

    before merge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,16 @@ class UsersController < ApplicationController
 
   def update
     @user= current_user
-    @user.update(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to root_path
+    if params[:first_name] == "" || params[:last_name] == ""
+      flash.now[:alert] = 'Erreur, vous devez remplir les champs'
+      render :edit
+    else
+      @user.update(first_name: params[:first_name], last_name: params[:last_name])
+      redirect_to root_path
+    end
+ 
   end
   
 end
+
+
