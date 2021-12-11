7b0665bc4bc89ad95420bd904a0c1d44d3c124be
talmidiel <talmidiel.officiel@gmail.com>
2021-12-11 17:49:20 +0100

    base setup for the paging script, and moved more code into partials

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,16 @@ class SearchesController < ApplicationController
 
   def index
     @keyword = params[:keyword]
-    @results = search(params)
+    @page = if params[:page].nil? || params[:page].to_i.zero?
+              1
+            else
+              params[:page].to_i
+            end
+
+    @results = search(params)[((@page - 1) * 6)..(@page * 6 - 1)]
+    respond_to do |format|
+      format.html { render :index }
+      format.js
+    end
   end
 end
