a5b354d9eca7f3ac46a551d720418555a23edf51
talmidiel <talmidiel.officiel@gmail.com>
2021-12-13 18:53:00 +0100

    added a timeout with a conditional for short messages

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -15,3 +15,8 @@ document.querySelector("#close").addEventListener("click", () => {
   fade(document.querySelector("#flash"));
 });
 
+if (document.querySelector('#flash-msg').textContent.length <= 15) {
+    setTimeout(() => {fade(document.querySelector("#flash"))}, 2250)
+} else {
+    setTimeout(() => {fade(document.querySelector("#flash"))}, (document.querySelector('#flash-msg').textContent.length * 150))
+}
