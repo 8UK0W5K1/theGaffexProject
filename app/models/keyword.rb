b3eda47c7f257b3e7dde925f3b61c62454bb3b00
talmidiel <talmidiel.officiel@gmail.com>
2021-12-14 17:59:23 +0100

    refactored the assign_keyword methos, and added a conditionnal to check if name already exists

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -2,11 +2,13 @@ class Keyword < ApplicationRecord
   has_many :keyword_to_articles, dependent: :destroy
   has_many :keywords, through: :keyword_to_articles
 
+  validates :name, presence: true, uniqueness: true
+
   def self.assign_keywords(params, article)
     params[:tags].join.split(';').each do |keyword|
-      @keyword = Keyword.new(name: keyword)
-      if @keyword.save
-        KeywordToArticle.create(article: article, keyword: @keyword)
+      if Keyword.find_by(name: keyword).nil?
+        @keyword = Keyword.new(name: keyword)
+        KeywordToArticle.create(article: article, keyword: @keyword) if @keyword.save
       else
         KeywordToArticle.create(article: article, keyword: Keyword.find_by(name: keyword))
       end
