470e4d8ddba7752d88ab1aa3565a5bac7bc1fe6a
talmidiel <talmidiel.officiel@gmail.com>
2021-12-14 17:09:46 +0100

    moved the fading fuction into a new file tp use it as a module

---
+++ app/assets/javascripts/fading.js
@@ -0,0 +1,25 @@
+function fade(element) {
+    let op = 1;
+    let timer = setInterval(function () {
+        if (op < 0.1) {
+            clearInterval(timer);
+            element.remove()
+        }
+        element.style.opacity = op;
+        element.style.filter = "alpha(opacity=" + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
+}
+
+function unFade(element) {
+    let op = 0.1;  // initial opacity
+    element.style.opacity = op;
+    let timer = setInterval(function () {
+        if (op >= 1) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
