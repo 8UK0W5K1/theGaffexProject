25a041fdb8b2e33c3a3ad73417e6fb81149897fd
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-14 10:01:27 +0100

    create show method in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def edit
     @user= current_user
