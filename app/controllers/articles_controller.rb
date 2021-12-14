b364734f98b04fd072eb982d3b95d8f3c57c2bce
8UK0W5K1 <jeremie.deldem@gmail.com>
2021-12-14 15:45:08 +0100

    install of wicked pdf + tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,12 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "file_name" , template: "articles/show.html.erb"  # Excluding ".pdf" extension.
+      end
+    end
   end
 
   def new; end
