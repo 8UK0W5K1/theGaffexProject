4960c2cb9ef71460c088ee178cc6aa7158073763
talmidiel <talmidiel.officiel@gmail.com>
2021-12-14 11:22:04 +0100

    fixed missing  h on through

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,4 +1,4 @@
 class Keyword < ApplicationRecord
   has_many :keyword_to_articles, dependent: :destroy
-  has_many :keywords, trough: :keyword_to_articles
+  has_many :keywords, through: :keyword_to_articles
 end
