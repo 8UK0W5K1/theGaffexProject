f096aa8b896abef0549c1c11312aabd3bb80605b
talmidiel <talmidiel.officiel@gmail.com>
2021-12-15 22:50:37 +0100

    changed more names inside the views and changed all functions and var names in js

--- app/assets/javascripts/keywords.js
+++ app/assets/javascripts/keywords.js
@@ -1,21 +1,21 @@
-const tagContainer = document.querySelector('.tag-container');
+const keywordContainer = document.querySelector('.keywords-container');
 const inputField = document.querySelector('#keyword-input');
-const tagArray = document.querySelector('#keywords-array')
+const keywordsArray = document.querySelector('#keywords-array')
 
-function getTags(tags) {
-    tags.split(';').forEach(e => {
-        const tag = newTag(e)
-        tagContainer.append(tag)
-        unFade(tag)
+function getKeywords(keywords) {
+    keywords.split(';').forEach(name => {
+        const keyword = newKeyword(name)
+        keywordContainer.append(keyword)
+        unFade(keyword)
         inputField.value = ''
-        tagArray.value += e + ';'
-        console.log(tagArray.value)
+        keywordsArray.value += name + ';'
+        console.log(keywordsArray.value)
     })
 }
 
-function newTag(name) {
+function newKeyword(name) {
     const div = document.createElement('div')
-    div.setAttribute('class', 'tag btn btn-primary mt-3 mx-1')
+    div.setAttribute('class', 'keyword btn btn-primary mt-3 mx-1')
     const span = document.createElement('span')
     span.innerHTML = name
     const close = document.createElement('span')
@@ -26,30 +26,30 @@ function newTag(name) {
     div.appendChild(close);
     div.addEventListener('click', (e) => {
         if (e.target.id === 'close') {
-            removeTag(div, name)
+            removeKeyword(div, name)
         }
     })
     return div
 }
 
-function removeTag(div, name) {
+function removeKeyword(div, name) {
     fade(div)
     setTimeout(() => {div.remove()}, 1500)
-    let tags = tagArray.value.split(';');
-    tags.splice(tags.indexOf(name), 1);
-    tagArray.value = tags.join(';')
-    console.log(tagArray.value)
+    let keywords = keywordsArray.value.split(';');
+    keywords.splice(keywords.indexOf(name), 1);
+    keywordsArray.value = keywords.join(';')
+    console.log(keywordsArray.value)
 }
 
 function init() {
     inputField.addEventListener('keyup', function (e) {
         if (e.key === ';') {
             const name = inputField.value.slice(0, -1)
-            const tag = newTag(name)
-            tagContainer.append(tag)
-            unFade(tag)
+            const keyword = newKeyword(name)
+            keywordContainer.append(keyword)
+            unFade(keyword)
             inputField.value = ''
-            tagArray.value += name + ';'
+            keywordsArray.value += name + ';'
         }
     })
 }
