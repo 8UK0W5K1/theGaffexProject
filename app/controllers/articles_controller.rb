c49eb2973aa5c2953769b485bee3aa49662c14cd
Lucie Caillat <lucie.caillat@gmail.com>
2021-12-15 18:03:56 +0100

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,6 +56,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(title: params[:title], summary: params[:summary], introduction: params[:introduction], protocol: params[:protocol], result: params[:result], conclusion: params[:conclusion], references: params[:references])
+    @article.attach_picture(params)
     redirect_to profile_path(current_user.id)
    
   end
